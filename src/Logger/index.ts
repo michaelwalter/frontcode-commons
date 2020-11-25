0ef755b03daba14d6d2d75789f2d51918bdaff7a
Michał Pacholski <pacholskimichal@gmail.com>
2020-11-25 22:06:09 +0100

    Initial commit

--- src/Logger/index.ts
+++ src/Logger/index.ts
@@ -1,11 +1,30 @@
 import log from 'loglevel';
 
+enum MODE {
+    DEFAULT = "DEFAULT",
+    DEBUG = "DEBUG",
+}
+
+
 class Logger {
-    debug (...message) {
-        log.debug(...message);
+    private _mode: MODE = MODE.DEFAULT;
+
+    debug (...message: any) {
+        console.log('... DEBUG ...');
+        if (this.mode === MODE.DEBUG) {
+            log.debug(...message);
+        }
     }
-    info (...message) {
+    info (...message: any) {
         log.info(...message);
+    }
+
+    set mode (type: MODE) {
+        this._mode = type;
+    }
+
+    get mode (): MODE {
+        return this._mode;
     }
 }
 
