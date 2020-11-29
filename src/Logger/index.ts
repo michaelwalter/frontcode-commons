93d6b2b88a24f084a7d278a673ba14ff08660f05
Michał Pacholski <pacholskimichal@gmail.com>
2020-11-29 22:24:40 +0100

    Initialize CLI with webpack and fastify

--- src/Logger/index.ts
+++ src/Logger/index.ts
@@ -1,31 +1,57 @@
-import log from 'loglevel';
+import chalk from 'chalk';
 
 enum MODE {
     DEFAULT = "DEFAULT",
     DEBUG = "DEBUG",
 }
 
-
-class Logger {
-    private _mode: MODE = MODE.DEFAULT;
-
-    debug (...message: any) {
-        console.log('... DEBUG ...');
-        if (this.mode === MODE.DEBUG) {
-            log.debug(...message);
-        }
-    }
-    info (...message: any) {
-        log.info(...message);
-    }
-
-    set mode (type: MODE) {
-        this._mode = type;
-    }
-
-    get mode (): MODE {
-        return this._mode;
-    }
+enum DEFAULTS {
+    LOCALES = 'pl-PL'
 }
 
-export default new Logger();
+export namespace Logger {
+
+    export const warn = (...message: any): void => {
+        console.log(chalk.yellow(...message));
+    }
+
+    export const info = (...message: any): void => {
+        console.log(...message)
+    }
+
+    export const debug = (...message: any): void => {
+        if (mode === MODE.DEBUG) {
+            const formatedDate = timeLogger.format(Date.now());
+            console.log(chalk.bold.white(`[${formatedDate}]`), ...message);
+        }
+    }
+
+    export const startDebugMode = (): void => {
+        mode = MODE.DEBUG
+    }
+
+    export const endDebugMode = (): void => {
+        mode = MODE.DEFAULT;
+    }
+
+    const getLocales = (): string => {
+        if (typeof navigator !== 'undefined') {
+            return (navigator.languages && navigator.languages[0]) || DEFAULTS.LOCALES;
+        } else {
+            return process.env.LANG || DEFAULTS.LOCALES;
+        }
+    }
+
+    const locales = getLocales();
+
+    const timeOptions = {
+        year: 'numeric', month: 'numeric', day: 'numeric',
+        hour: 'numeric', minute: 'numeric', second: 'numeric',
+        hour12: false,
+    }
+
+    const timeLogger =  new Intl.DateTimeFormat(locales, timeOptions);
+
+    let mode = MODE.DEFAULT;
+
+}
